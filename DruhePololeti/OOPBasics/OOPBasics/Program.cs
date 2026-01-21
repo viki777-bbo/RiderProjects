b46ef70ec06160efd6c05b05d0c5bda80fba233e
Viktor <viktor.bacik@academicschool.cz>
2026-01-21 09:39:48 +0100

    Added druhe pololeti OOPBasics

---
+++ DruhePololeti/OOPBasics/OOPBasics/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Student student = new Student(16, "Viktor");
+Console.WriteLine("Student name:" + student.name);
+student.Greet();
+ChangeStudent(student);
+student.Greet();
+
+void ChangeStudent(Student s)
+{
+    s.name = "Nothing";
+}
+
+
